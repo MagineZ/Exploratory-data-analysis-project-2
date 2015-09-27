3b34f8f24e5c8d9c60a530204133cde10f70034b
MagineZ <b94401079@gmail.com>
2015-09-26 23:47:40 -0700

    plot6

--- plot6.R
+++ plot6.R
@@ -1,15 +1,15 @@
-#setwd("./exdata-data-NEI_data")
-#NEI <- readRDS("summarySCC_PM25.rds")
-#SCC <- readRDS("Source_Classification_Code.rds")
-#vehicles <- grepl("Vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
-#vehiclesSCC <- SCC[vehicles,]$SCC
-#vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
-#library(ggplot2)
+setwd("./exdata-data-NEI_data")
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+vehicles <- grepl("Vehicle", SCC$SCC.Level.Two, ignore.case=TRUE)
+vehiclesSCC <- SCC[vehicles,]$SCC
+vehiclesNEI <- NEI[NEI$SCC %in% vehiclesSCC,]
+library(ggplot2)
 subsetNEI <- vehiclesNEI[vehiclesNEI$fips=="24510"|vehiclesNEI$fips=="06037",  ]
 aggregatedTotalByYearAndFips <- aggregate(Emissions ~ year+fips, subsetNEI, sum)
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="24510"] <- "Baltimore, MD"
 aggregatedTotalByYearAndFips$fips[aggregatedTotalByYearAndFips$fips=="06037"] <- "Los Angeles, CA"
-png("plot6.png", width=1040, height=480)
+png("plot6.png", width=480, height=480)
 g <- ggplot(aggregatedTotalByYearAndFips, aes(factor(year), Emissions))
 g <- g + facet_grid(. ~ fips) + geom_bar(stat="identity") +xlab("year") +
     ylab(expression('Total PM'[2.5]*" Emissions")) +
